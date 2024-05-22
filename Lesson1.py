57f924e631a3c6e2b83536d7b7a7a94a2080b3e5
antony oduor <116315398+oduoranto@users.noreply.github.com>
2024-05-22 15:26:48 +0300

    Tip Calculator

--- Lesson1.py
+++ Lesson1.py
@@ -5,4 +5,5 @@ tip = int(input("How much tip would you like to give? 10,12 or 15: "))
 split = int(input("How many people to split the bill? : "))
 total = bill - tip
 totaSplit = round(total/split,2)
+
 print (f"Everyone will contribute: ${totaSplit}")
