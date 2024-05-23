6ef35292f15420b97ee899af8a10351884656f4f
antony oduor <116315398+oduoranto@users.noreply.github.com>
2024-05-23 09:34:10 +0300

    Banker Roulette

---
+++ Exercise45.py
@@ -0,0 +1,13 @@
+import random
+print("Welcome to the Banker Roulette")
+
+names = ["Anto", "Johnny", "Kevo", "Eugo", "Emma"]
+number = random.randint(0,4)
+print( "The person paying is : " + names[number])
+
+
+names1 = names1_string.split("a, b , c, d")
+items = len(names1)
+
+rand = random.randint(0, items-1)
+print (names1[rand])
