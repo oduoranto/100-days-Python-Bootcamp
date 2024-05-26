f63df6371dc18d5da4b98eec3a871920f5c01b2c
antony oduor <116315398+oduoranto@users.noreply.github.com>
2024-05-26 11:28:51 +0300

    The Hurdles loop challange

---
+++ Exercise_61.py
@@ -0,0 +1,22 @@
+print ("Welcome to the hurdles challange")
+
+def move():
+    print("Moving>>")
+
+def turn_left():
+    print("turning left")
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+move()
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()     
+turn_left()
+move()
